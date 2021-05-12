31e6ad18823f88b0818cf8e19a3aca36bb4b0fa0
HelloYmf <60568618+HelloYmf@users.noreply.github.com>
2021-05-12 20:19:56 +0800

    Add files via upload

---
+++ ProcessMonitor/View Module/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MainView.rc 使用
+//
+#define IDD_DIALOG_MAIN                 101
+#define IDC_LIST_PROCESS                1007
+#define IDC_LIST_FUNCTION               1010
+#define IDC_EDIT_DLLPATH                1011
+#define IDC_BUTTON_CHOOSEFILE           1012
+#define IDC_BUTTON_INJECT               1013
+#define IDC_BUTTON_STARTMONITOR         1014
+#define IDC_BUTTON_REMOTECALL           1015
+#define IDC_BUTTON_REFRESH              1016
+#define IDC_EDIT_IMPORTFUNCTION         1017
+#define IDC_BUTTON_FUNCTIONOK           1018
+#define IDC_EDIT_CHOOSEPROCESS          1019
+#define IDC_BUTTON_STARTMONITOR2        1020
+#define IDC_BUTTON_INLINEHOOKSTART      1020
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
